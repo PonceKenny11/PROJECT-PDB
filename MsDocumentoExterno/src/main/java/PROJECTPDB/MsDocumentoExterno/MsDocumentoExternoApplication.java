336ecfb4cbe2c21c2f9f788c3ae2eafe7c9b69ee
Gustavo Ponce <132284919+PonceKenny11@users.noreply.github.com>
2023-12-15 20:49:42 -0500

    Ingresando Un nuevo Microservicio
    Micro Servicio donde se realizara un CRUD del documento externo del sistema de tramite documentario

---
+++ MsDocumentoExterno/src/main/java/PROJECTPDB/MsDocumentoExterno/MsDocumentoExternoApplication.java
@@ -0,0 +1,13 @@
+package PROJECTPDB.MsDocumentoExterno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDocumentoExternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDocumentoExternoApplication.class, args);
+	}
+
+}
---
+++ MsDocumentoExterno/src/test/java/PROJECTPDB/MsDocumentoExterno/MsDocumentoExternoApplicationTests.java
@@ -0,0 +1,13 @@
+package PROJECTPDB.MsDocumentoExterno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDocumentoExternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
