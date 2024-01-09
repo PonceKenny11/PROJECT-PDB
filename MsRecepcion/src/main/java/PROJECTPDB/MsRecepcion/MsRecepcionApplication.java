e30d0560c6bfd9d33f9742baaca7ed351332511f
MICHAELORTIZ29 <michaelosielo@gmail.com>
2024-01-08 22:10:47 -0500

    crecion del nuevo micreoservicio

---
+++ MsRecepcion/src/main/java/PROJECTPDB/MsRecepcion/MsRecepcionApplication.java
@@ -0,0 +1,13 @@
+package PROJECTPDB.MsRecepcion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRecepcionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRecepcionApplication.class, args);
+	}
+
+}
---
+++ MsRecepcion/src/test/java/PROJECTPDB/MsRecepcion/MsRecepcionApplicationTests.java
@@ -0,0 +1,13 @@
+package PROJECTPDB.MsRecepcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRecepcionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
